c70a5ac68d2a6078ac48fc2322232b668026f96c
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-03-19 20:54:47 +0530

    add order feature

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -18,15 +18,23 @@ const addorder = async (req, res) => {
 
 const getorder = async (req, res) => {
   try {
-    console.log(req.userId);
-      if (!req.userId) {
+    if (!req.userId) {
       return res.status(401).json({ message: "User not authenticated" });
     }
-  
-    const orders = await order.find({ userId: req.userId }); // Fetch all items in cart collection
+
+    let orders;
+
+    if (req.role === "admin") {
+      // Admin sees all orders
+      orders = await order.find();
+    } else {
+      // Normal user sees only their orders
+      orders = await order.find({ userId: req.userId });
+    }
+
     res.status(200).json(orders);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
     res.status(500).json({ message: err.message });
   }
 };
