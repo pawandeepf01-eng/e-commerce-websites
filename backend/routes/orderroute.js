2ea115f9257bd11a537de05467df3a3bbd3da6f8
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-03-19 18:35:00 +0530

    add order ferture

--- backend/routes/orderroute.js
+++ backend/routes/orderroute.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 
-const  {addorder,getorder}=require("../controller/order.js")
+const  {addorder,getorder,admingetorder}=require("../controller/order.js")
 
 const { authMiddleware } = require("../middlware/auth.js");
 
 
 
 router.post("/addorder",authMiddleware, addorder);
-router.get("/order", authMiddleware,getorder)
+router.get("/order", authMiddleware, getorder)
+router.get("/adminorder", authMiddleware,admingetorder)
+
 
 
 
